cbd699b93bb988684e3d78511df1b417fbc45bf7
Aidyl <jgarciaalbalah@gmail.com>
2022-05-15 10:00:53 -0400

    main: + debugShowCheckedModeBanner

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Rick and Morty Character Wiki',
+      debugShowCheckedModeBanner: true,
       theme: _buildTheme(),
       home: Container(),
     );
