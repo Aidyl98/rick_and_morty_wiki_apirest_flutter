291ef5ffa516c36d9618f0aceafbdb43549da56b
Aidyl <jgarciaalbalah@gmail.com>
2022-05-15 14:08:04 -0400

    + CharacterImageWidget: + CircleAvatar

---
+++ lib/ui/widget/character_image_widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CharacterImageWidget extends StatelessWidget {
+  final String characterImage;
+  final double radiusImage;
+
+  const CharacterImageWidget({
+    Key? key,
+    required this.characterImage,
+    this.radiusImage = 30,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      radius: radiusImage,
+      child: ClipOval(
+        child: Image.network(
+          characterImage,
+          fit: BoxFit.contain,
+          loadingBuilder: (context, widget, imageChunkEvent) {
+            return imageChunkEvent == null
+                ? widget
+                : const CircularProgressIndicator();
+          },
+        ),
+      ),
+      backgroundColor: Theme.of(context).colorScheme.primary,
+    );
+  }
+}
