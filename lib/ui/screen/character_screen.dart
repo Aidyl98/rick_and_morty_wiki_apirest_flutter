713636b31d9a551712a06e496fc6a7a97201fb8c
Aidyl <jgarciaalbalah@gmail.com>
2022-05-15 16:46:40 -0400

    + DisplayCharacterScreen

---
+++ lib/ui/screen/character_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:rick_and_morty_wiki_apirest_flutter/app_exporter.dart';
+import 'package:rick_and_morty_wiki_apirest_flutter/ui/widget/character_body.dart';
+
+class DisplayCharacterScreen extends StatelessWidget {
+  const DisplayCharacterScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) =>
+          CharacterBloc(characterRepository: CharacterServiceImplementation())
+            ..add(const CharacterFetchEvent()),
+      child: Scaffold(
+        appBar: AppBar(
+          title: AutoSizeText(
+            'Rick and Morty Character Wiki',
+            style: Theme.of(context).textTheme.headline6,
+            maxLines: 1,
+          ),
+        ),
+        body: const CharacterBody(),
+      ),
+    );
+  }
+}
