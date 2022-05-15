851385f6c27146bab7664a463342e6c73050236b
Aidyl <jgarciaalbalah@gmail.com>
2022-05-15 16:22:14 -0400

    + WidgetUtils

---
+++ lib/ui/widget/widget_utils.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+
+class WidgetUtils {
+  static buildInfoText({
+    required String text,
+    required BuildContext context,
+    required Size size,
+    TextAlign? textAlign,
+    Color? color,
+  }) {
+    return AutoSizeText(
+      text,
+      style: Theme.of(context).textTheme.subtitle1?.copyWith(
+            fontSize: size.width / 15,
+            color: color,
+          ),
+      textAlign: textAlign,
+    );
+  }
+
+  static buildIndicatorText(
+    String text,
+    BuildContext context,
+    Size size,
+  ) {
+    return AutoSizeText(
+      text,
+      style: Theme.of(context).textTheme.subtitle2?.copyWith(
+            fontSize: size.width / 14,
+          ),
+    );
+  }
+
+  static buildCircularProgressIndicator(BuildContext context) {
+    return CircularProgressIndicator(
+      color: Theme.of(context).colorScheme.secondary,
+      strokeWidth: 8,
+    );
+  }
+}
