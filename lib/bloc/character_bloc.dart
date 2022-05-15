e657935ec622e3d8aed1e132f129d0a1fa0e7371
Aidyl <jgarciaalbalah@gmail.com>
2022-05-15 10:46:14 -0400

    CharacterBloc: + mapEventToState()

--- lib/bloc/character_bloc.dart
+++ lib/bloc/character_bloc.dart
@@ -1,13 +1,33 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:rick_and_morty_wiki_apirest_flutter/repository/repository_exporter.dart';
 
 part 'character_event.dart';
 part 'character_state.dart';
 
 class CharacterBloc extends Bloc<CharacterEvent, CharacterState> {
-  CharacterBloc() : super(CharacterInitial()) {
-    on<CharacterEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final CharacterServiceImplementation characterRepository;
+  int page = 1;
+  bool isFetching = false;
+
+  CharacterBloc({
+    required this.characterRepository,
+  }) : super(CharacterInitial());
+
+  @override
+  Stream<CharacterState> mapEventToState(CharacterEvent event) async* {
+    if (event is CharacterFetchEvent) {
+      try {
+        yield const CharacterLoadingState(message: 'Loading Characters');
+        final response = await characterRepository.getAllCharacters(page);
+        yield CharacterSuccessState(
+          character: response.toList(),
+        );
+        page++;
+      } on Exception {
+        yield const CharacterErrorState(
+            error: "Couldn't fetch characters. Is the device online?");
+      }
+    }
   }
 }
