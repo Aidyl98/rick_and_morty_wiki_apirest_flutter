a44c19ad5cca6217a4421252de86a70613bf1d9c
Aidyl <jgarciaalbalah@gmail.com>
2022-05-15 10:14:48 -0400

    + CharacterModel autogenerated

---
+++ lib/repository/model/character_model.g.dart
@@ -0,0 +1,54 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'character_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CharacterModel _$CharacterModelFromJson(Map<String, dynamic> json) =>
+    CharacterModel(
+      id: json['id'] as int,
+      name: json['name'] as String,
+      status: json['status'] as String,
+      species: json['species'] as String,
+      type: json['type'] as String,
+      gender: json['gender'] as String,
+      origin:
+          CharacterLocation.fromJson(json['origin'] as Map<String, dynamic>),
+      location:
+          CharacterLocation.fromJson(json['location'] as Map<String, dynamic>),
+      image: json['image'] as String,
+      episode:
+          (json['episode'] as List<dynamic>).map((e) => e as String).toList(),
+      url: json['url'] as String,
+      created: DateTime.parse(json['created'] as String),
+    );
+
+Map<String, dynamic> _$CharacterModelToJson(CharacterModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'status': instance.status,
+      'species': instance.species,
+      'type': instance.type,
+      'gender': instance.gender,
+      'origin': instance.origin,
+      'location': instance.location,
+      'image': instance.image,
+      'episode': instance.episode,
+      'url': instance.url,
+      'created': instance.created.toIso8601String(),
+    };
+
+CharacterLocation _$CharacterLocationFromJson(Map<String, dynamic> json) =>
+    CharacterLocation(
+      name: json['name'] as String,
+      url: json['url'] as String,
+    );
+
+Map<String, dynamic> _$CharacterLocationToJson(CharacterLocation instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'url': instance.url,
+    };
