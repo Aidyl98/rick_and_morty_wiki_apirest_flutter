50df2b72655566730051250fcaf8bb28f19ac5b3
Aidyl <jgarciaalbalah@gmail.com>
2022-05-18 17:46:14 -0400

    CharacterServiceImplementation: + handle Exception

--- lib/repository/service/character_service_impl.dart
+++ lib/repository/service/character_service_impl.dart
@@ -22,8 +22,18 @@ class CharacterServiceImplementation extends CharacterService {
 
       return List<CharacterModel>.from(
           allEntities.map((x) => CharacterModel.fromJson(x)));
-    } on DioError {
-      rethrow;
+    } on DioError catch (exc) {
+      if (exc.response != null) {
+        if (exc.response!.statusCode == 404) {
+          throw Exception("You have reached the end of the character list.");
+        } else {
+          throw Exception(exc.response!.statusCode.toString() +
+              ": " +
+              exc.response!.statusMessage.toString());
+        }
+      } else {
+        throw Exception("Couldn't fetch characters. Is the device online?");
+      }
     }
   }
 }
